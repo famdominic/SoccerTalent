f82f0f7be8a8af89a132ba42d251f46fc6c6bcb6
famdominic <phamdominic2007@gmail.com>
2026-09-24 18:59:59 -0400

    Refactor write statements in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
 
-st.write(
-    "Finding the best soccer stars")
-)
+st.write("Finding the best soccer stars")
+
